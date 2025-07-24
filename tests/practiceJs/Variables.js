6795fb5d274cf40c71b5886a3824bd69bff544d4
Bipin Thapa <bit@logpoint.com>
2025-07-24 17:14:23 +0545

    1st

---
+++ tests/practiceJs/Variables.js
@@ -0,0 +1,16 @@
+let name="Bipin"
+let age=25;
+let isStudent=true;
+let hobbies=["reading", "coding", "gaming"];
+let address={city:"Kathmandu", country:"Nepal"};
+
+function displayInfo() {
+    console.log("Name:" +name);
+    console.log("Name:", name);
+    console.log("Age:", age);
+    console.log("Is Student:", isStudent);
+    console.table(hobbies);
+
+}
+
+displayInfo();
